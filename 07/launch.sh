ad59f318bd65674605185565b33c3c98dd06d14c
cxq <chmath@126.com>
2024-05-29 09:24:35 +0800

    07 mnmc

--- 07/launch.sh
+++ 07/launch.sh
@@ -6,6 +6,7 @@ export MASTER_ADDR="172.18.0.11" #hostname if /etc/hosts add
 export MASTER_PORT="12355"
 torchrun --nnodes=2 \
          --nproc_per_node=2 \
+         --max-restarts=3 \
          --rdzv_id=100 \
          --rdzv_backend=c10d \
          --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} \
